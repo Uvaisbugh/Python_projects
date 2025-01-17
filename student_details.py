b8ce109aa61a61fcd0c96f1d647ab11b45015768
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-17 19:45:03 +0530

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -19,5 +19,23 @@ class Student_details:
         else:
             print("Grade: F")
             
+    def update_marks(self, new_marks):
+        self.total_marks = new_marks
     
+    def marklist(self):
+        print(f"Name: {self.name}")
+        print(f"Roll Number: {self.roll_no}")
+        print(f"Total Marks: {self.total_marks}")
         
+        
+faris=Student_details("faris",1,80)
+faris.display()
+faris.update_marks(90)
+faris.display()
+faris.marklist()
+
+Shamsul=Student_details("Shamsul",2,70)
+Shamsul.display()
+Shamsul.update_marks(90)
+Shamsul.display()
+Shamsul.marklist()
