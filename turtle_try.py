688e9b368959d7e18c024f39ebe8542a2bc30763
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-27 21:51:11 +0530

    Create turtle_try.py

---
+++ turtle_try.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+t1 = Turtle()
+t2 = Turtle()
+
+t1.color('red')
+t2.color('blue')
+
+t1.backward(100)
+t2.forward(80)
+
+t1.left(90)
+t2.left(90)
+
+t1.forward(100)
+t2.forward(100)
+
+t1.right(90)
+t2.left(90)
+
+t1.forward(50)
+t2.forward(50)
+
+t1.right(90)
+t2.left(90)
+
+t1.forward(130)
+t2.forward(130)
+
+done()
